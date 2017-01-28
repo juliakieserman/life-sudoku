7120a947c12ed4882c5705274065ba47633c53a7
Julia Kieserman <jbk67@georgetown.edu>
2017-01-28 15:47:16 -0600

    Initial Commit

---
+++ life sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  life sudoku
+//
+//  Created by Julia Kieserman on 1/28/17.
+//  Copyright © 2017 Julia Kieserman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
