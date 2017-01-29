e854f92ba91b1bdc5f2ee038b9602043ada7fff9
Julia Kieserman <jbk67@georgetown.edu>
2017-01-29 16:41:29 -0600

    added scroll option picker

--- life sudoku/ViewController.swift
+++ life sudoku/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource,UIPickerViewDelegate {
+    
+    @IBOutlet weak var categoryPicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.categoryPicker.dataSource = self
+        self.categoryPicker.delegate = self
+        
+           pickerData = ["sleep", "work", "exercise"]
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 }
 
