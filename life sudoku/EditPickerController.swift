fd1617665af5716374117352a2984ff2d3150dac
Julia Kieserman <jbk67@georgetown.edu>
2017-02-04 19:59:59 -0600

    swipe to delete an item from the data picker

--- life sudoku/EditPickerController.swift
+++ life sudoku/EditPickerController.swift
@@ -8,12 +8,23 @@
 
 import UIKit
 
-class EditPickerController: UIViewController {
+class EditPickerController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var pickerTableView: UITableView!
+    let cellReuseIdentifier = "cell"
+    
+    
+    var pickerData: [String] = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        pickerData = ["sleep", "work", "exercise"]
+
         // Do any additional setup after loading the view.
+        self.pickerTableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+        pickerTableView.delegate = self
+        pickerTableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,6 +32,45 @@ class EditPickerController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+
+    
+    // number of rows in table view
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.pickerData.count
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell:UITableViewCell = self.pickerTableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell!
+        
+        // set the text from the data model
+        cell.textLabel?.text = self.pickerData[indexPath.row]
+        
+        return cell
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You tapped cell number \(indexPath.row).")
+    }
+    
+    // this method handles row deletion
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == .delete {
+            
+            // remove the item from the data model
+            pickerData.remove(at: indexPath.row)
+            
+            // delete the table view row
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            
+        } else if editingStyle == .insert {
+            // Not used in our example, but if you were adding a new row, this is where you would do it.
+        }
+    }
 
     /*
     // MARK: - Navigation
